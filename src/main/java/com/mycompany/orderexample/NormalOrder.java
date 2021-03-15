ba43cb0c856f2a97e7b648b1a067412479887932
dazwfd14 <dstones@portlaoisecollege.ie>
2021-03-15 09:59:15 +0000

    Upload to Git

---
+++ src/main/java/com/mycompany/orderexample/NormalOrder.java
@@ -0,0 +1,45 @@
+package com.mycompany.orderexample;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class NormalOrder extends Order {
+
+    /**
+     * Default constructor
+     */
+    public NormalOrder() {
+    }
+
+    /**
+     * 
+     */
+    public void confirm() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void dispatch() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void receive() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void close() {
+        // TODO implement here
+    }
+
+}
---
+++ src/main/java/com/mycompany/orderexample/SpecialOrder.java
@@ -0,0 +1,38 @@
+package com.mycompany.orderexample;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class SpecialOrder extends Order {
+
+    /**
+     * Default constructor
+     */
+    public SpecialOrder() {
+    }
+
+    /**
+     * 
+     */
+    public void confirm() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void close() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void dispatch() {
+        // TODO implement here
+    }
+
+}
