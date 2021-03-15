8c457144be56b9bad4b15b36e05730123d273187
dazwfd14 <dstones@portlaoisecollege.ie>
2021-03-15 09:35:01 +0000

    Upload to Git

---
+++ src/main/java/com/mycompany/orderexample/Customer.java
@@ -0,0 +1,44 @@
+package com.mycompany.orderexample;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Customer {
+  
+
+    /**
+     * 
+     */
+    private String name;
+
+    /**
+     * 
+     */
+    private String location;
+    
+    /**
+     * Default constructor
+     */
+    public Customer() {
+        
+    }
+   
+
+    /**
+     * 
+     */
+    public void sendOrder() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void receiveOrder() {
+        // TODO implement here
+    }
+
+}
---
+++ src/main/java/com/mycompany/orderexample/Order.java
@@ -0,0 +1,42 @@
+package com.mycompany.orderexample;
+
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Order {
+
+    /**
+     * Default constructor
+     */
+    public Order() {
+    }
+
+    /**
+     * 
+     */
+    private Date date;
+
+    /**
+     * 
+     */
+    private String number;
+
+
+    /**
+     * 
+     */
+    public void confirm() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void close() {
+        // TODO implement here
+    }
+
+}
